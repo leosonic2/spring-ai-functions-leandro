3999368a329586ad4e9a182b8989e93eb6373bf4
Leandro Martins Fernandes <br05756175+prosegur@prosegur.com>
2026-04-16 10:31:02 -0300

    (class 1)- Add Weather API integration with request and response models

---
+++ src/main/java/guru/springframework/springaifunctions/functions/WeatherServiceFunction.java
@@ -0,0 +1,48 @@
+package guru.springframework.springaifunctions.functions;
+
+import guru.springframework.springaifunctions.model.WeatherRequest;
+import guru.springframework.springaifunctions.model.WeatherResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.client.RestClient;
+
+import java.util.function.Function;
+@Slf4j
+public class WeatherServiceFunction implements Function<WeatherRequest, WeatherResponse> {
+
+    public static final String WEATHER_URL = "https://api.api-ninjas.com/v1/weather";
+
+    private final String apiNinjasKey;
+
+    public WeatherServiceFunction(@Value("${api-ninjas.api-key}") String apiNinjasKey) {
+        this.apiNinjasKey = apiNinjasKey;
+    }
+
+    @Override
+    public WeatherResponse apply(WeatherRequest weatherRequest) {
+        RestClient restClient = RestClient
+                .builder()
+                .baseUrl(WEATHER_URL)
+                .defaultHeaders(httpHeaders->{
+                    httpHeaders.set("X-Api-Key", apiNinjasKey);
+                    httpHeaders.set("Accept", "application/json");
+                    httpHeaders.set("Content-Type", "application/json");
+                })
+                .build();
+
+        return restClient.get().uri(uriBuilder -> {
+            log.info("Building URI for weather request: {}", weatherRequest);
+
+            uriBuilder.queryParam("city", weatherRequest.location());
+
+            if (weatherRequest.state() != null && !weatherRequest.state().isBlank()) {
+                uriBuilder.queryParam("state", weatherRequest.state());
+            }
+            if (weatherRequest.country() != null && !weatherRequest.country().isBlank()) {
+                uriBuilder.queryParam("country", weatherRequest.country());
+            }
+            return uriBuilder.build();
+        }).retrieve().body(WeatherResponse.class);
+    }
+
+}
